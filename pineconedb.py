ae87c56b672d254ac393debf3da3d426f45e9da0
daniel ung <daniel.ung@sjsu.edu>
2024-02-29 14:11:33 -0800

    small typo

--- pineconedb.py
+++ pineconedb.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-pc = Pinecone(api_key=os.getenv("INECONE_API_KEY"))
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 index = pc.Index("paywithface")
